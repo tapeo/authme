8ea26c9e7da189ff8d122f0356a10dd3298d6c27
Matteo Ricupero <info@matteoricupero.it>
2025-03-27 10:05:24 +0100

    feat(model): add userModel export to index

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,6 +2,7 @@ export { default as OAuthState, type IOAuthState } from "./oauth-state.model";
 export { default as OTP, OtpPurpose, type IOtp } from "./otp.model";
 export {
   User,
+  userModel,
   UserSchema,
   type User as IUser,
   type RefreshToken,
